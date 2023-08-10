e345621adb14919b439ea6345a4c8c15ed792925
jackkhc <hancjacky@gmail.com>
2023-08-09 22:50:29 -0400

    delete todos functionality in server

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.get("/todos/:userEmail", async (req, res) => {
     const {userEmail} = req.params
 
     try {
-        const todos = await pool.query('SELECT * FROM todos WHERE user_email = $1',[userEmail])
+        const todos = await pool.query('SELECT * FROM todos WHERE user_email = $1;',[userEmail])
         res.json(todos.rows)
     } catch (error) {
         console.error(error)
@@ -23,8 +23,20 @@ app.get("/todos/:userEmail", async (req, res) => {
 app.post("/todos",(req,res) => {
     const { id, name, date, user_email } = req.body
     try {
-        pool.query(`INSERT INTO todos(id, name, date, user_email) VALUES ($1, $2, $3, $4)`,[id, name, date, user_email])
+        pool.query(`INSERT INTO todos(id, name, date, user_email) VALUES ($1, $2, $3, $4);`,[id, name, date, user_email])
         console.log('todo added')
+    }
+    catch {
+        console.error(err)
+    }
+})
+
+//delete todos
+app.delete("/todos/:id",(req,res) => {
+    const { id } = req.params
+    try {
+        pool.query(`DELETE FROM todos WHERE id = $1;`,[id])
+        console.log('todo deleted')
     }
     catch {
         console.error(err)
